313fb0b5e6eb15066f808c2049b5d60f7a9b46c3
Diego D'Ortenzio <diegodorte2007@gmail.com>
2026-02-26 10:55:42 +0100

    Add files via upload

--- wsphp/api/index.php
+++ wsphp/api/index.php
@@ -4,24 +4,26 @@ header("Access-Control-Allow-Methods: OPTIONS, GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 header("Access-Control-Max-Age: 3600");
 
+//Parametri richiesta
 $metodo = $_SERVER["REQUEST_METHOD"];
+$content_type = $_SERVER["CONTENT_TYPE"] ?? null;
+if ($content_type !== null) {
+    $content_type = explode(';', $content_type)[0];
+}
 $formatoRichiesto = $_SERVER['HTTP_ACCEPT'] ?? 'application/json';
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $array_url_diviso = explode('/', trim($uri, '/'));
-$posUser = array_search('user', $array_url_diviso);
+$posApi = array_search('api', $array_url_diviso); //http://localhost/wsphp/api/{id - opzionale}
 
-if ($posUser === false) {
-    inviaRisposta(404, ["errore" => "Scrivi /user"], $formatoRichiesto);
-}
+if ($posApi === false) inviaRisposta(404, ["errore" => "Url in formato non corretto"], $formatoRichiesto);
 
-$id=null; //Nella mia idea cè user/id
-if(isset($array_url_diviso[$posUser + 1])){
-    $id=$array_url_diviso[$posUser+1];
+$id=null;
+if(isset($array_url_diviso[$posApi + 1])){
+    $id=$array_url_diviso[$posApi+1];
 }
 
 $dbJSON = leggiFile();
-
 switch ($metodo) {
     case "GET":
         if ($id !== null) {
@@ -30,25 +32,33 @@ switch ($metodo) {
             } else {
                 inviaRisposta(404, ["errore" => "Id inesistente"], $formatoRichiesto);
             }
+        } else {
+            inviaRisposta(200, $dbJSON, $formatoRichiesto);
         }
-        inviaRisposta(200, $dbJSON, $formatoRichiesto);
         break;
-
     case "POST":
         $body = file_get_contents('php://input');
-        $nuovoUtente = json_decode($body, true);
 
-        $risultatoValida = validaDati($nuovoUtente);
-        if ($risultatoValida !== true) inviaRisposta(400, ["errore" => $risultatoValida], $formatoRichiesto);
+        $nuovoUtente=null;
+        if($content_type == 'application/json') 
+            $nuovoUtente = json_decode($body, true);
+        elseif($content_type == 'application/xml'){
+            $xml = simplexml_load_string($body);
+            $json = json_encode($xml);
+            $nuovoUtente = json_decode($json, true);
+        }else{
+            inviaRisposta(400, ["errore" => "Formato Non Supportato"], $formatoRichiesto);
+        }
 
-        if (!$nuovoUtente) inviaRisposta(400, ["errore" => "Dati non inseriti"], $formatoRichiesto);
+        if (!$nuovoUtente || (!isset($nuovoUtente["name"]) || !isset($nuovoUtente["age"]) || !isset($nuovoUtente["date"]))) inviaRisposta(400, ["errore" => "Dati non inseriti"], $formatoRichiesto);
         else{
             if(empty($dbJSON)) $nuovoId = 0;
             else $nuovoId = max(array_keys($dbJSON)) + 1;
             
             $dbJSON[$nuovoId] = $nuovoUtente;
             scriviNelFile($dbJSON);
-            inviaRisposta(201, ["id" => $nuovoId, "messaggio" => "Creato con successo"], $formatoRichiesto);
+
+            inviaRisposta(201, ["id" => $nuovoId, "obj" => $nuovoUtente], $formatoRichiesto);
         }
         break;
 
@@ -56,23 +66,32 @@ switch ($metodo) {
         if ($id === null || !isset($dbJSON[$id])) inviaRisposta(404, ["errore" => "ID mancante o utente inesistente"], $formatoRichiesto);
 
         $body = file_get_contents('php://input');
-        $nuoviDatiUtente = json_decode($body, true); 
+        $nuoviDatiUtente=null;
+        if($content_type == 'application/json') 
+            $nuoviDatiUtente = json_decode($body, true);
+        elseif($content_type == 'application/xml'){
+            $xml = simplexml_load_string($body);
+            $json = json_encode($xml);
+            $nuoviDatiUtente = json_decode($json, true);
+        }else{
+            inviaRisposta(400, ["errore" => "Formato Non Supportato"], $formatoRichiesto);
+        }
         
-        $risultatoValida = validaDati($nuoviDatiUtente);
-        if ($risultatoValida !== true) inviaRisposta(400, ["errore" => $risultatoValida], $formatoRichiesto);
-
-        if (!$nuoviDatiUtente) inviaRisposta(400, ["errore" => "Body non valido"], $formatoRichiesto);
-        
-        $dbJSON[$id] = $nuoviDatiUtente;
-        scriviNelFile($dbJSON);
-        inviaRisposta(200, ["messaggio" => "Utente aggiornato"], $formatoRichiesto);
+        if (!$nuoviDatiUtente || (!isset($nuoviDatiUtente["name"]) || !isset($nuoviDatiUtente["age"]) || !isset($nuoviDatiUtente["date"]))) inviaRisposta(400, ["errore" => "Body non valido"], $formatoRichiesto);
+        else{
+            $dbJSON[$id] = $nuoviDatiUtente;
+            scriviNelFile($dbJSON);
+            inviaRisposta(200, ["messaggio" => "Utente aggiornato"], $formatoRichiesto);
+        }
         break;
 
     case "DELETE":
         if ($id === null || !isset($dbJSON[$id])) inviaRisposta(404, ["errore" => "Impossibile eliminare: ID non trovato"], $formatoRichiesto);
-        unset($dbJSON[$id]);
-        scriviNelFile($dbJSON);
-        inviaRisposta(200, ["messaggio" => "Utente eliminato"], $formatoRichiesto);
+        else{
+            unset($dbJSON[$id]);
+            scriviNelFile($dbJSON);
+            inviaRisposta(200, ["messaggio" => "Utente eliminato"], $formatoRichiesto);
+        }
         break;
 
     default:
@@ -107,8 +126,7 @@ function arrayToXml($data, &$xmlData) {
 
 function inviaRisposta($codice, $payload, $formato) {
     http_response_code($codice);
-    
-    if (strpos($formato, 'application/xml') !== false) {
+    if ($formato == "application/xml") {
         header("Content-Type: application/xml; charset=UTF-8");
         $xml = new SimpleXMLElement('<root/>');
         arrayToXml($payload, $xml);
@@ -118,17 +136,4 @@ function inviaRisposta($codice, $payload, $formato) {
         echo json_encode($payload);
     }
     exit;
-}
-
-function validaDati($data) {
-    $campiObbligatori = ['nome', 'email'];
-    
-    foreach ($campiObbligatori as $campo) {
-        if (!isset($data[$campo]) || empty(trim($data[$campo]))) {
-            return "Campo '$campo' mancante o vuoto.";
-        }
-    }
-    if (count($data) > count($campiObbligatori)) return "Richiesta contiene campi non permessi.";
-    if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) return "Formato email non valido.";
-    return true; 
 }
